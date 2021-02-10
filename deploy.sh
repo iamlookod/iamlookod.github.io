5531ebe1856d740c3817e6765c17dbf69fdba1d7
precharcDev <precharc@scg.com>
2021-02-10 23:21:04 +0700

    Ui: add vuetify

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:iamlookod/iamlookod.github.io.git main:gh-pages
+git push -f git@github.com:iamlookod/iamlookod.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# git push -f git@github.com:iamlookod/iamlookod.github.io.git main:gh-pages
 
 cd -
